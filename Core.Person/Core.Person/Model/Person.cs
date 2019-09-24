8df7a0b9d49cab0bb52e5380a3c5d47baf24ecc6
Junio Silva <junio.silvac@gmail.com>
2019-09-23 22:42:09 -0300

    Add interface Person

--- Core.Person/Core.Person/Model/Person.cs
+++ Core.Person/Core.Person/Model/Person.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Core.Person.Model
+﻿namespace Core.Person.Model
 {
     public class Person
     {
         public long Id { get; set; }
 
         public string FirstName { get; set; }
-       
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+
     }
 }
---
+++ Core.Person/Core.Person/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Core.Person.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+
+        public Model.Person Create(Model.Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Model.Person> FindAll()
+        {
+            var persons = new List<Model.Person>();
+            for(int  i = 0; i < 8; i++)
+            {
+                var person = MockPerson(i);
+                persons.Add(person);
+            }
+
+            return persons;
+        }
+
+        public Model.Person FindById(long id)
+        {
+            return new Model.Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Junio",
+                LastName = "Silva",
+                Address = "Centro",
+                Gender = "Male"
+
+            };
+        }
+
+        public Model.Person Update(Model.Person person)
+        {
+            return person;
+        }
+
+        public Model.Person MockPerson(int i)
+        {
+            return new Model.Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Junio" + i,
+                LastName = "Silva" + i,
+                Address = "Centro" + i,
+                Gender = "Male"
+
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
